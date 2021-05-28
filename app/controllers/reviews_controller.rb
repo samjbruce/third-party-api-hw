a878168443e2a37c79b6801c72e23e94cee653b1
samjbruce <samueljamesbruce@gmail.com>
2021-05-28 10:50:59 -0500

    adds secure api key

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   def index
-    response = HTTP.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=#{params[:search]}&api-key=WjfxfGsEwbMcQ7ZJFAobbKpaK4ZReP1q")
-    key = nyt.access_key
-    render json: key
+    response = HTTP.get("https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=#{params[:search]}&api-key=#{Rails.application.credentials.nyt_api_key}")
+  
+    render json: response.parse(:json)
   end
 end
